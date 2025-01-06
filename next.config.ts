1c17492bc0853facdc117caea72b78be5b3671da
HarryDang <danghuuphuc001@gmail.com>
2025-01-06 22:52:55 +0700

    feat(deploying): config something to deploying project

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  rules: {
+    "no-console": "off", // Example: Disable `no-console` rule
+    "react/no-unescaped-entities": "off", // Disable warning for unescaped entities
+    // Add or modify rules as needed
+  },
 };
 
 export default nextConfig;

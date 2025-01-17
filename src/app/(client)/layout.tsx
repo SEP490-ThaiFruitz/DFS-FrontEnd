2380a5cd1ff563bef04c9b030ce08fca6097507f
HarryDang <danghuuphuc001@gmail.com>
2025-01-17 23:59:30 +0700

    chore: change something

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -10,15 +10,15 @@ interface ClientLayoutProps {
 const ClientLayout = ({ children, test1, test2 }: ClientLayoutProps) => {
   return (
     <>
-      <div className="flex items-center mx-4">
+      <div className="flex items-center mx-4 fixed top-0">
         <Logo />
         <Navbar />
       </div>
-      <div className="flex flex-col">
+      <div className="flex flex-col pt-20 h-full">
         {children}
 
-        <div>{test1}</div>
-        <div>{test2}</div>
+        {/* <div>{test1}</div>
+        <div>{test2}</div> */}
       </div>
     </>
   );
--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -1,14 +1,25 @@
+import { ImagesSlider } from "@/components/global-components/images-slider";
 import Image from "next/image";
 
 const ClientPage = () => {
+  const images = [
+    "/images/first-background.jpg",
+    "/images/third-background.png",
+  ];
+
   return (
-    <div className="text-center flex items-center justify-center bg-yellow-100">
-      <Image
-        src="/images/dried-fruit.webp"
-        alt="Picture of the author"
-        width={500}
-        height={500}
-      />
+    <div className="text-center h-full w-full flex items-center justify-center bg-yellow-100">
+      <ImagesSlider images={images}>
+        <div className="flex flex-col items-center gap-1 mr-2">
+          <Image
+            src="/images/dried-fruit.webp"
+            alt="Picture of the author"
+            width={50}
+            height={50}
+          />
+          <h1 className="text-slate-700 font-serif italic">ThaiFruitz</h1>
+        </div>
+      </ImagesSlider>
     </div>
   );
 };

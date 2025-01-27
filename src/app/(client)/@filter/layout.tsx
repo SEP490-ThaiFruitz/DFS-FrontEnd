b8a779e70f84d26686e226cbbafe8740f640a09b
HarryDang <danghuuphuc001@gmail.com>
2025-01-27 19:10:58 +0700

    feat(home-page): metadata for page

--- src/app/(client)/@filter/layout.tsx
+++ src/app/(client)/@filter/layout.tsx
@@ -1,5 +1,15 @@
+import { Metadata } from "next";
+
 interface LayoutFilterProps {
   children: React.ReactNode;
+}
+
+export async function generateMetadata() {
+  return {
+    title: "Trang chủ",
+    description:
+      "Thai Fruitz - Trang chủ nơi bạn có thể tìm thấy những sản phẩm organic tươi ngon nhất, giup bạn duy trì một lối sống lành mạnh.",
+  };
 }
 
 const LayoutFilter = ({ children }: LayoutFilterProps) => {

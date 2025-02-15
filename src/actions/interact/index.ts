724f4a7b549a68f43d3a0ea9d513a1fe64647816
HarryDang <danghuuphuc001@gmail.com>
2025-02-15 10:46:28 +0700

    fix(build): failed build step

--- src/actions/interact/index.ts
+++ src/actions/interact/index.ts
@@ -1,10 +1,16 @@
-export const getF = async (endpoint: string) => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_URL_API}${endpoint}`);
+export const getF = async <T>(endpoint: string): Promise<T | any> => {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_URL_API}${endpoint}`
+    );
 
-  if (!response.ok) {
-    console.log("response", response);
+    if (!response.ok) {
+      console.log("response", response);
 
-    return undefined;
+      return undefined;
+    }
+    return await response.json();
+  } catch (error) {
+    console.log({ error });
   }
-  return response.json();
 };

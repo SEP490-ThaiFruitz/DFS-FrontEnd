272c48f9043de48b707fc805b4089a28f8ce9b41
Harry Dang <danghuuphuc001@gmail.com>
2025-03-28 23:09:49 +0700

    chore: adjust something

--- src/providers/signalr-provider.tsx
+++ src/providers/signalr-provider.tsx
@@ -1,80 +1,93 @@
 "use client";
 
-import { getProfile } from '@/actions/user';
-import { ApiResponse, Profile } from '@/types/types';
-import { HubConnection, HubConnectionBuilder, HubConnectionState } from '@microsoft/signalr';
-import { useQuery } from '@tanstack/react-query';
-import { ReactNode, useCallback, useEffect, useRef } from 'react';
-import { toast } from 'sonner';
+import { getProfile } from "@/actions/user";
+import { ApiResponse, Profile } from "@/types/types";
+import {
+  HubConnection,
+  HubConnectionBuilder,
+  HubConnectionState,
+} from "@microsoft/signalr";
+import { useQuery } from "@tanstack/react-query";
+import { ReactNode, useCallback, useEffect, useRef } from "react";
+import { toast } from "sonner";
 
 type Props = {
-    children: ReactNode;
-}
+  children: ReactNode;
+};
 
 interface Payment {
-    isSuccess: boolean,
-    orderId: string,
-    userId: string,
+  isSuccess: boolean;
+  orderId: string;
+  userId: string;
 }
 
 export default function SignalRProvider({ children }: Props) {
-    const connection = useRef<HubConnection | null>(null);
-    const { data: user } = useQuery({
-        queryKey: ["authUser"],
-        queryFn: async () => {
-            try {
-                const res = await getProfile();
-                if (res?.isSuccess) {
-                    const data: ApiResponse<Profile> = res?.data;
-                    return data.value;
-                }
-                return null;
-            } catch (error) {
-                console.log(error);
-                toast.error("Lỗi hệ thống");
-            }
-        },
-        retry: false,
-        initialData: null,
-    });
-
-    const notifyUrl = process.env.NODE_ENV !== "production"
-        ? process.env.NEXT_PUBLIC_NOTIFY_URL!
-        : "https://api.hamazaki.online/hubs/notifications";
-
-
-    const handlePayment = useCallback((payment: Payment) => {
-        console.log("PaymentReturn", payment)
-        if (user?.id === payment.userId) {
-            return toast.success(payment.orderId)
+  const connection = useRef<HubConnection | null>(null);
+  const { data: user } = useQuery({
+    queryKey: ["authUser"],
+    queryFn: async () => {
+      try {
+        const res = await getProfile();
+        if (res?.isSuccess) {
+          const data: ApiResponse<Profile> = res?.data;
+          return data.value;
         }
-    }, [user?.id])
+        return null;
+      } catch (error) {
+        console.log(error);
+        toast.error("Lỗi hệ thống");
+      }
+    },
+    retry: false,
+    initialData: null,
+  });
 
-    useEffect(() => {
-        if (!connection.current) {
-            connection.current = new HubConnectionBuilder()
-                .withUrl(notifyUrl)
-                .withAutomaticReconnect()
-                .build();
+  const notifyUrl = process.env.NEXT_PUBLIC_NOTIFY_URL!;
 
-            connection.current.start()
-                .then(() => 'Connected to notification hub')
-                .catch(err => console.log(err));
-        }
+  const handlePayment = useCallback(
+    (payment: Payment) => {
+      //   console.log("PaymentReturn", payment);
+      if (user?.id === payment.userId) {
+        return toast.success(payment.orderId);
+      }
+    },
+    [user?.id]
+  );
 
-        connection.current.on('ReceivePayment', handlePayment);
-        if (connection.current && connection.current.state == HubConnectionState.Connected) {
-            if (user) {
-                connection.current.send('RegistHook', user.id, user.role, connection.current.connectionId)
-                    .catch((error) => console.log(error));
-            }
-        }
+  useEffect(() => {
+    if (!connection.current) {
+      connection.current = new HubConnectionBuilder()
+        .withUrl(notifyUrl)
+        .withAutomaticReconnect()
+        .build();
 
-        return () => {
-            connection.current?.off('ReceivePayment', handlePayment);
-        }
+      connection.current
+        .start()
+        .then(() => "Connected to notification hub")
+        .catch((err) => console.log(err));
+    }
 
-    }, [user, notifyUrl, handlePayment])
+    connection.current.on("ReceivePayment", handlePayment);
+    if (
+      connection.current &&
+      connection.current.state == HubConnectionState.Connected
+    ) {
+      if (user) {
+        connection.current
+          .send(
+            "RegistHook",
+            user.id,
+            user.role,
+            connection.current.connectionId
+          )
+          .catch((error) => console.log(error));
+      }
+    }
 
-    return children;
+    return () => {
+      connection.current?.off("ReceivePayment", handlePayment);
+    };
+  }, [user, notifyUrl, handlePayment]);
+
+  return children;
 }

76dfb14474b77943b0919bca9c0e8e613b0640da
Harry Dang <danghuuphuc001@gmail.com>
2025-04-27 02:07:49 +0700

    refactor: implement data from api

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -8,6 +8,7 @@ import dynamic from "next/dynamic";
 import { AppleCardBlogsCarousel } from "@/features/client/home/apple-card-carousel-blogs";
 import { AnimatedTestimonials } from "@/components/global-components/aceternity/animated-testimonial";
 import { placeholderImage } from "@/utils/label";
+import { API } from "../key/url";
 // import { ProductFilterSidebarContainer } from "@/components/custom/filter-product-sidebar/product-filter-sidebar-container";
 
 type Feedback = {
@@ -169,7 +170,7 @@ const ProductFilterSidebarContainer = dynamic(() =>
   ).then((mod) => mod.ProductFilterSidebarContainer)
 );
 
-const ClientPage = () => {
+const ClientPage = async () => {
   const images = [
     "/images/first-background.jpg",
     "/images/third-background.png",
@@ -185,6 +186,28 @@ const ClientPage = () => {
     "/marque/image-8.avif",
   ];
 
+  const rawFeedbackData = await fetch(`${API}/Feedbacks/feedback-home`, {
+    cache: "force-cache",
+  }).then((res) => res.json());
+
+
+  const feedbackData: Feedback[] =
+    rawFeedbackData?.value?.map(
+      (
+        item: Omit<Feedback, "createdAt"> & { createdOnUtc: string },
+        index: number
+      ) => ({
+        id: index,
+        user: {
+          name: item.user.name,
+          avatar: item.user.avatar || placeholderImage,
+        },
+        content: item.content,
+        rating: item.rating,
+        createdAt: item.createdOnUtc,
+      })
+    ) ?? [];
+    
   return (
     <>
       <div className="flex flex-col h-full overflow-hidden">
@@ -196,9 +219,14 @@ const ClientPage = () => {
 
         <CategorySlide />
 
-        <div className="container mx-auto mt-10 mb-10  gap-4 px-4 md:px-0">
-          <AnimatedTestimonials testimonials={feedbacks} autoplay />
-        </div>
+        {feedbackData.length > 0 && (
+          <div className="container mx-auto mt-10 mb-10  gap-4 px-4 md:px-0">
+            <h1 className="my-4 text-center text-4xl font-bold font-sans ">
+              Phản hồi từ khách hàng
+            </h1>
+            <AnimatedTestimonials testimonials={feedbackData} autoplay />
+          </div>
+        )}
 
         <AppleCardBlogsCarousel />
 
